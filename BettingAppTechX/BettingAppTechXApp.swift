9094bcdb26b508a24fc6c800af7b7123e0a97a9a
Mihir Sharma <mailid4manu@gmail.com>
2023-12-18 23:43:15 -0800

    Initial Commit

---
+++ BettingAppTechX/BettingAppTechXApp.swift
@@ -0,0 +1,17 @@
+//
+//  BettingAppTechXApp.swift
+//  BettingAppTechX
+//
+//  Created by Mihir Sharma on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BettingAppTechXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
